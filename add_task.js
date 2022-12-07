9cb249136e4babd450c6fc0c826f6c51aac4f449
Leon9708 <Leon.ring@gmx.de>
2022-12-07 22:50:45 +0100

    addtaskstructure

--- add_task.js
+++ add_task.js
@@ -1,27 +1,32 @@
 const dropdown = document.getElementById("dropdown");
-
-const selectButtonTask2 = document.getElementById('selectButtonTask2');
 const dropdown2 = document.getElementById("dropdown2");
-const options2 = document.querySelectorAll(".option2");
-const selectLabel2 = document.getElementById('selectLabel2');
-
+let priority;
 let colorID;
-
 let categoryLabels = []
-let tasks = [
-    {}
-]
+let subtaskID = 0;
+let subtasks = []
+
 
 function createTask() {
     let title = document.getElementById('inputTitle').value;
     let description = document.getElementById('inputDescription').value;
     let category = document.getElementById('selectedLabel').innerText;
+    let due_date = document.getElementById('inputDate').value;
     let label = categoryLabels.filter(function(ele) {
         return ele.label === category
     })
     let color = label.color;
+    let task = {
+        "title": title,
+        "description": description,
+        "categories": [{
+            "title": label,
+            "color": color
+        }],
+        "prority": priority,
+        "due_date": date,
 
-    let task = { "title": title, "description": description, "category": category, "color": color, }
+    }
 }
 
 //first Select sectionx
@@ -51,7 +56,6 @@ function openInputCategory() {
 
 function createNewCategory() {
     let newLabel = document.querySelector('.input_category').value;
-
     categoryLabels.push({ "label": newLabel, "color": colorID });
     unsetNewCategory();
 
@@ -65,12 +69,7 @@ function createNewCategory() {
 function addCategory() {
     document.getElementById('addCategory').innerHTML = "";
     for (let i = 0; i < categoryLabels.length; i++) {
-        document.getElementById('addCategory').innerHTML += `
-        <div id="${categoryLabels[i].label}" onclick="selectTitle(this.id)" class="box_categoryelement">
-        <input class="option">
-            <label  class="select-item">${categoryLabels[i].label}</label>
-            <div id="categoryelement_color${i}" class="button_color categoryelement_color"></div>
-        </div>`
+        document.getElementById('addCategory').innerHTML += addCategoryHTML()
         document.getElementById('categoryelement_color' + i).style.backgroundColor = categoryLabels[i].color;
     }
 }
@@ -107,30 +106,33 @@ function clickedColor(id) {
 
 //second Select section
 
-selectButtonTask2.addEventListener("click", function(e) {
-    e.preventDefault();
-    toggleHidden2();
-});
-
-function toggleHidden2() {
+function toggleDropdown2() {
     if (dropdown.classList.contains('hidden')) {
         dropdown2.classList.toggle('hidden');
     }
 }
 
-options2.forEach(function(option) {
-    option.addEventListener("click", function(e) {
-        setSelectTitle2(e);
-    });
-});
-
-function setSelectTitle2(e) {
-    const labelElement2 = document.querySelector(`label[for="${e.target.id}"]`).innerText;
-    selectLabel2.innerText = labelElement2;
-    toggleHidden2();
+function selectUser(e) {
+    let selectedUser = contacts.filter(function(ele) {
+        return ele.id === +e
+    })
+    document.getElementById('selectedUser').innerHTML = `${selectedUser[0].firstName} ${selectedUser[0].name}`;
+    toggleDropdown2();
 };
 
-//Choose Prio 
+function loadUser() {
+    document.getElementById('addUser').innerHTML = "";
+    for (let i = 0; i < contacts.length; i++) {
+        const contact = contacts[i];
+        document.getElementById('addUser').innerHTML += ` 
+        <div id="${contact.id}" onclick="selectUser(this.id)" class="box_categoryelement">
+            <input type="radio" name="where" value="internet" class="option">
+            <label id="newCategory2" class="select-item">${contact.firstName} ${contact.name}</label>
+        </div>`
+    }
+}
+
+//choose Prio 
 
 function selectUrgency(prio) {
     if (prio === 'urgent') {
@@ -146,39 +148,81 @@ function selectUrgency(prio) {
 }
 
 function urgent() {
-    const urgent = document.getElementById("PrioUrgent");
+    const urgent = document.getElementById("prioUrgent");
     document.getElementById('urgentButton').classList.toggle('box_button_task_u');
     urgent.classList.toggle('urgency_img_u_clicked_task');
     urgent.classList.toggle('urgency_img_u_task');
+    priority = "urgent";
 }
 
 function medium() {
-    const medium = document.getElementById("PrioMedium");
+    const medium = document.getElementById("prioMedium");
     document.getElementById('mediumButton').classList.toggle('box_button_task_m');
     medium.classList.toggle('urgency_img_m_clicked_task');
     medium.classList.toggle('urgency_img_m_task');
+    priority = "medium";
 }
 
 function low() {
-    const low = document.getElementById("PrioLow");
+    const low = document.getElementById("prioLow");
     document.getElementById('lowButton').classList.toggle('box_button_task_l');
     low.classList.toggle('urgency_img_l_clicked_task');
     low.classList.toggle('urgency_img_l_task');
+    priority = "low";
+}
+
+//subtask
+
+function openSubtask() {
+    document.getElementById('containerSubtask').innerHTML = openSubtaskHTML();
+    createInputSubtask();
+}
+
+function newSubtask() {
+    let newSubtask = document.querySelector('.input_subtask').value;
+    document.getElementById('containerSubtask').innerHTML = unsetSubtaskHTML();
+    subtaskID++;
+    console.log('newSubtask', newSubtask)
+    subtasks.push({
+        "id": subtaskID,
+        "subtask": newSubtask
+    })
+    loadSubtask();
+}
+
+function loadSubtask() {
+    document.getElementById('boxSubtasks').innerHTML = "";
+    for (let i = 0; i < subtasks.length; i++) {
+        const subtask = subtasks[i];
+        document.getElementById('boxSubtasks').innerHTML += subtasksHTML(subtask);
+    }
+}
+
+function createInputSubtask() {
+    x = document.createElement('INPUT');
+    x.setAttribute('class', 'input_subtask');
+    x.setAttribute('type', 'text');
+    x.setAttribute('placeHolder', 'Add new subtask');
+    x.setAttribute('maxLength', '25');
+    x.setAttribute('minLength', '3')
+    x.setAttribute('id', 'inputSubtask')
+    document.getElementById('subtaskInput').appendChild(x);
 }
 
 function unsetPrioHTML() {
-    document.getElementById('containerButtonsTask').innerHTML = `<button id="urgentButton" onclick="selectUrgency('urgent')" class="box_button_task ">
-    <p class="text_urgency_task">Urgent</p>
-    <div id="PrioUrgent" class="urgency_img_u_task urgency_img_task"></div>
-</button>
-<button id="mediumButton" onclick="selectUrgency('medium')" class="box_button_task  ">
-    <p class="text_urgency_task ">Medium</p>
-   <div id="PrioMedium" class="urgency_img_m_task urgency_img_task"></div>
-</button>
-<button id="lowButton" onclick="selectUrgency('low')" class="box_button_task ">
-    <p class="text_urgency_task ">Low</p>
-  <div id="PrioLow" class="urgency_img_l_task urgency_img_task"></div>
-</button>   `
+    document.getElementById('containerButtonsTask').innerHTML = `
+    <button id="urgentButton" onclick="selectUrgency('urgent')" class="box_button_task ">
+        <p class="text_urgency_task">Urgent</p>
+        <div id="prioUrgent" class="urgency_img_u_task urgency_img_task"></div>
+    </button>
+    <button id="mediumButton" onclick="selectUrgency('medium')" class="box_button_task  ">
+        <p class="text_urgency_task ">Medium</p>
+    <div id="prioMedium" class="urgency_img_m_task urgency_img_task"></div>
+    </button>
+    <button id="lowButton" onclick="selectUrgency('low')" class="box_button_task ">
+        <p class="text_urgency_task ">Low</p>
+    <div id="prioLow" class="urgency_img_l_task urgency_img_task"></div>
+    </button>   `
 }
 
 function generateNewCategoryHTML() {
@@ -203,7 +247,8 @@ function generateNewCategoryHTML() {
 }
 
 function generateBoxButtonsHTML() {
-    return `     <button onclick="clickedColor(this.id)" id="blue" class="button_color color_1"></button>
+    return `     
+    <button onclick="clickedColor(this.id)" id="blue" class="button_color color_1"></button>
     <button onclick="clickedColor(this.id)" id="red" class="button_color color_2"></button>
     <button onclick="clickedColor(this.id)" id="green" class="button_color color_3"></button>
     <button onclick="clickedColor(this.id)" id="orange" class="button_color color_4"></button>
@@ -213,7 +258,6 @@ function generateBoxButtonsHTML() {
 
 
 function unsetNewCategoryHTML() {
-
     return `    
     <button onclick="toggleDropdown()" id="selectButtonTask" class="select_button_task">
         <span class="select_label" id="selectedLabel">Select task category</span>
@@ -221,4 +265,52 @@ function unsetNewCategoryHTML() {
         <div id="arrow" class="arrow"></div>
     </button>`
 
+}
+
+function addCategoryHTML() {
+    return `
+        <div id="${categoryLabels[i].label}" onclick="selectTitle(this.id)" class="box_categoryelement">
+        <input class="option">
+            <label  class="select-item">${categoryLabels[i].label}</label>
+            <div id="categoryelement_color${i}" class="button_color categoryelement_color"></div>
+        </div>`
+}
+
+function openSubtaskHTML() {
+    return ` 
+    <div class="box_input_category">
+        <div id="subtaskInput" class="place_input_category"></div>
+        <div class="box_buttons_input_category">
+            <button onclick="unsetNewCategory()"  class="button_input_category">
+                <img class="img_button_input_category" src="assets/img/cross_task.png" alt="#">
+            </button>
+            <div class="seperation_buttons_input_category"></div>
+            <button onclick="newSubtask()" class="button_input_category">
+                <img class="img_button_input_category" src="assets/img/check_task.png" alt="#">
+            </button>
+        </div>
+    </div>`
+}
+
+function unsetSubtaskHTML() {
+    return `
+        <button onclick="openSubtask()" id="boxSubtaskInput" class="box_subtask_input">
+            <input  disabled="disabled" placeholder="Add new subtask" id="inputSubtask" type="text" class="input_subtask">
+            <div  class="button_subtask_input">
+                <img class="img_subtask_task" src="./assets/img/plus_task.png" alt="#">
+            </div>
+        </button>
+   `
+}
+
+function subtasksHTML(subtask) {
+    return `  
+    <div class="box_create_subtask">
+        <label class="box_checkbox">
+            <input type="checkbox" >
+            <span class="checkmark"></span>
+        </label>
+        <p class="subtask" id="subtask${subtask.subtaskID}">${subtask.subtask}</p>
+    </div> 
+    `
 }
