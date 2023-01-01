f0466e5dde35c7b3433702dd34f28e58699bc4cf
Leon9708 <Leon.ring@gmx.de>
2023-01-02 00:19:21 +0100

    update contacts

--- contacts.js
+++ contacts.js
@@ -12,9 +12,9 @@ async function getContacts() {
 
 
 async function renderContacts() {
-    await getTodos();
-    await showContacts();
-    console.log('contacts', contacts)
+    /*  await getTodos(); */
+    await filterLetters();
+    showContacts();
 }
 
 
@@ -25,64 +25,70 @@ let contacts = [{
     'firstName': 'Adam',
     'email': 'ABC@gmail.com',
     'phone': '123',
+    'color': ''
 }, {
     'id': 1,
     'lastName': 'Bertaberta',
     'firstName': 'Berta',
     'email': 'BGI@gmail.com',
     'phone': '257',
+    'color': ''
 }, {
     'id': 2,
     'lastName': 'Charlie',
     'firstName': 'ABC',
     'email': 'CGI@gmail.com',
     'phone': '357',
+    'color': ''
 }, {
     'id': 3,
     'lastName': 'Doradora',
     'firstName': 'Dora',
     'email': 'DGI@gmail.com',
     'phone': '457',
+    'color': ''
 }, {
     'id': 4,
     'lastName': 'Evaeva',
     'firstName': 'Eva',
     'email': 'EFG@gmail.com',
     'phone': '543',
+    'color': ''
 }];
+/* let randomColor = colors[Math.floor(Math.random() * colors.length)] */
+let colors = ['orange', 'purple', 'blue', 'red', 'aqua', 'brown', 'grey', 'green'];
+let letters = [];
 
+async function filterLetters() {
+    contacts.forEach(contact => {
+        if (!letters.includes(contact.firstName.charAt(0))) {
+            letters.push(contact.firstName.charAt(0))
+        }
+    });
+    showLetters();
+}
 
-function showContacts() {
-    let contactList = document.getElementById('contactList');
-    contactList.innerHTML = '';
-    for (let i = 0; i < contacts.length; i++) {
-        const singleContact = contacts[i];
-        contactList.innerHTML += showContactsHTML(singleContact, i);
-        /*showFirstLetterName(i);*/
+function showLetters() {
+    document.getElementById('contactList').innerHTML = "";
+    for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        document.getElementById('contactList').innerHTML += generateLetters(letter);
     }
 }
 
+function showContacts() {
+    for (let i = 0; i < contacts.length; i++) {
+        const singleContact = contacts[i];
 
-function showContactsHTML(singleContact, i) {
-    return `
-    <button class="contact_card_button" onclick="showSelectedContact(${i})">
-        <div class="contact_list_container">
-            <div class="contact_list_img"><h3>${singleContact['name'].charAt(0)}</h3></div>
-            <div class="contact_list">
-                <div id="contact" class="contact_list_content">
-                    <div><b>${singleContact['name']}</b></div>
-                </div>
-                <div class="contact_list_content">
-                    <div>${singleContact['email']}</div>
-                </div>
-            </div>
-        </div>
-    </button>    
-    `
+        let singleContactLetter = singleContact.firstName.charAt(0);
+        let id = "boxContact" + singleContactLetter;
+
+
+        /*    document.getElementById('circle' + i).style.backgroundColor = singleContact.color; */
+        document.getElementById(id).innerHTML += showContactsHTML(singleContact, i);
+    }
 }
 
-
-// Show Contact Form to enter new Contact
 function openContactForm(text) {
     document.getElementById('contactForm').classList.remove('d_none');
     document.getElementById('contactContent').innerHTML = text;
@@ -93,23 +99,61 @@ function closeContactForm() {
     document.getElementById('contactForm').classList.add('d_none');
 }
 
-
-// Show SELECTED CONTACT
 function showSelectedContact(i) {
-    let contactCard = document.getElementById('contactCard');
-    contactCard.innerHTML = '';
-    let selectedContact = contacts[i];
-    contactCard.innerHTML += showSelectedContactHTML(selectedContact);
+    const contactCardButtons = document.querySelectorAll('.contact_card_button');
+    const contactNames = document.querySelectorAll('.contact_name');
+
+    for (let j = 0; j < contactCardButtons.length; j++) {
+        contactCardButtons[j].style.backgroundColor = 'white';
+        contactNames[j].style.color = 'black';
+    }
+
+    document.getElementById('contactCard').innerHTML = '';
+    contactCard.innerHTML += showSelectedContactHTML(contacts[i]);
+
+    document.getElementById("buttonContact" + i).style.backgroundColor = "#2A3647"
+    document.getElementById("contactName" + i).style.color = "white"
 }
 
+function showContactsHTML(singleContact, i) {
+    return `
+    <div id="containerCard" class="container_cards">
+        <button id="buttonContact${i}" class="contact_card_button" onclick="showSelectedContact(${i})">
+            <div class="contact_list_container">
+                <div id="circle${i}" class="contact_list_img">
+                    <p>${singleContact['firstName'].charAt(0)}${singleContact['lastName'].charAt(0)}</p>
+                </div>
+                <div class="contact_content">
+                    <div id="contact" class="contact_list_name">
+                        <p class="contact_name" id="contactName${i}">${singleContact['firstName']} ${singleContact['lastName']}</p>
+                    </div>
+                    <div class="contact_list_email">
+                        <p>${singleContact['email']}</p>
+                    </div>
+                </div>
+            </div>
+        </button>
+    </div>
+    `
+}
+
+
+function generateLetters(letter) {
+    return `   
+    <div id="boxContact${letter}" class="box_contact">
+        <div class="contact_letter">
+            <h3>${letter}</h3>
+        </div>
+    </div>`
+}
 
 function showSelectedContactHTML(selectedContact) {
     return `
     <div class="contact_list_container">
         <div class="contact_list">
             <div class="contact_list_content">
-                <div class="contact_list_img"><h3>${selectedContact['name'].charAt(0)}</h3></div>
-                <div><h3>${selectedContact['name']}</h3></div>
+                <div class="show_contact_img"><h3>${selectedContact['firstName'].charAt(0)}${selectedContact['lastName'].charAt(0)}</h3></div>
+                <div><h3>${selectedContact['lastName']}</h3></div>
             </div>
             <div><h4>Contact Information</h4></div>
             <div class="contact_list_content">
