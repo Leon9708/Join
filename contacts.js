c8f4d07283385da73e453afc4265c4ab7ea641a2
Leon9708 <Leon.ring@gmx.de>
2023-01-03 13:18:06 +0100

    update contacts

--- contacts.js
+++ contacts.js
@@ -1,3 +1,17 @@
+async function includeHTML() {
+    let includeElements = document.querySelectorAll('[w3-include-html]');
+    for (let i = 0; i < includeElements.length; i++) {
+        const element = includeElements[i];
+        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        let resp = await fetch(file);
+        if (resp.ok) {
+            element.innerHTML = await resp.text();
+        } else {
+            element.innerHTML = 'Page not found';
+        }
+    }
+}
+
 async function getContacts() {
     try {
         let responseServer = await fetch('https://jonas34.pythonanywhere.com/todos/', { method: 'GET', headers: { 'Content-Type': 'application/json', } });
